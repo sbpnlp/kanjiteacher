39ccb99eba5010eaa9c207f672f9378ca5fefa2f
steven.poggel <steven.poggel@2dca3a74-c63a-11de-81ce-f10158adbdd1>
2009-11-09 14:45:28 +0000

    New Webservice proj

---
+++ Webservice2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KSvc = Kanji.KanjiService;
+
+namespace Kanji.Webservice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KSvc.Service serv = new KSvc.Service();
+            bool b = true;
+            serv.Run((object)b);
+        }
+    }
+}
