905c044e421e5269f9de1bb09a3fe57bad8f1e8a
steven.poggel <steven.poggel@2dca3a74-c63a-11de-81ce-f10158adbdd1>
2009-11-11 22:09:18 +0000

    Handing an integer over to Web service - instead it should be a visitor (the controller of the desktop app) that will be notified, when data has been received. The KanjiService will be consumed by the controller, just like the BLL.

--- trunk/KanjiService/KanjiService.cs
+++ trunk/KanjiService/KanjiService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ServiceModel;
 
 namespace Kanji.KanjiService
 {
@@ -13,17 +14,21 @@ namespace Kanji.KanjiService
         InputCharacter = 0x00000008
     }
 
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class KanjiService : IKanjiService
     {
         private int InputAreaMessages { get; set; }
 
         private int DesktopMessages { get; set; }
+        
+        public int ANumber { get; set; }
 
         #region IKanjiService Members
 
         public bool ReceivePoints(List<int> xcoords, List<int> ycoords, List<DateTime> times)
         {
             Console.WriteLine("Received mouse event: x:{0},y:{1} received datetime: {2}", xcoords[0], ycoords[0], times[0].ToShortDateString());
+            Console.WriteLine("And the data coming from outside is {0}", ANumber);
             return true;
         }
 
