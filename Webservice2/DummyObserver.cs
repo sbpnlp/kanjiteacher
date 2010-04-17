fe306fab88d28645a7c19e578fc63acf4ed134d4
steven.poggel <steven.poggel@2dca3a74-c63a-11de-81ce-f10158adbdd1>
2010-04-17 06:22:54 +0000

    [Code] Communication. Reasonable state.

--- Webservice2/DummyObserver.cs
+++ Webservice2/DummyObserver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Kanji.DesktopApp.Interfaces;
+using System.IO;
 
 namespace Kanji.Webservice2
 {
@@ -22,12 +23,27 @@ namespace Kanji.Webservice2
         {
             Console.WriteLine("This is the Dummy Observer.");
 //            Console.WriteLine(string.Format("Received a list of points at {0}", times[times.Count-1].ToLongTimeString()));
+            using (StreamWriter sw = new StreamWriter("out.txt", true))
+            {
+                //xxx HACK the last element of ActiveTimes is not a time but the stroke number
+                sw.WriteLine(string.Format("Received a list of points [{2}] at {0}.\n\tLatency since drawing of last point (mouseup event): {1}",
+                    DateTime.Now.ToLongTimeString(),
+                    new DateTime(DateTime.Now.Ticks - times[times.Count - 2].Ticks).ToLongTimeString(),
+                    times[times.Count - 1].Ticks));
 
-            //xxx HACK the last element of ActiveTimes is not a time but the stroke number
-            Console.WriteLine(string.Format("Received a list of points [{2}] at {0}.\n\tLatency since drawing of last point (mouseup event): {1}", 
-                DateTime.Now.ToLongTimeString(), 
-                new DateTime(DateTime.Now.Ticks - times[times.Count - 2].Ticks).ToLongTimeString(),
-                times[times.Count-1].Ticks));
+                sw.Write(string.Format("\tStroke[{0}]: ", times[times.Count - 1].Ticks));
+                for (int i = 0; i < xcoords.Count; i++)
+                {
+                    sw.Write(string.Format("<{0},{1}> ", xcoords[i], ycoords[i]));
+                }
+                sw.WriteLine();
+
+
+                Console.WriteLine(string.Format("Received a list of points [{2}] at {0}.\n\tLatency since drawing of last point (mouseup event): {1}",
+                    DateTime.Now.ToLongTimeString(),
+                    new DateTime(DateTime.Now.Ticks - times[times.Count - 2].Ticks).ToLongTimeString(),
+                    times[times.Count - 1].Ticks));
+            }
         }
 
         #endregion
